92df5649b3f95bbd11b7e5338aef2d5dfab7f0a7
root <root@ip-172-31-31-37.us-east-2.compute.internal>
2017-06-23 22:41:47 +0000

    first commit

---
+++ flaskapp.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify
+import boto3
+import json
+import simplejson as json1
+import random
+app = Flask(__name__)
+
+APP_ROOT=os.path.dirname(os.path.abspath(__file__))
+
+adjs = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+    "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+    "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+    "wandering", "withered", "wild", "black", "young", "holy", "solitary",
+    "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+    "polished", "ancient", "purple", "lively", "nameless"]
+nouns = [
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+    "frog", "smoke", "star"]
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+@app.route('/generator', methods=['GET'])
+def generator():
+	n = len(adjs)
+	m = len(nouns)
+	project_name = adjs[int(random.random() * n)] + " " + nouns[int(random.random() * m)]
+	return project_name
+
+if __name__ == '__main__':
+  app.run()
